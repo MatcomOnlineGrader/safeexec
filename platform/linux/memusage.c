e08df29780fb5447bfc9c3824b869254e9f8e390
Leandro Castillo Valdes <sulkybay@gmail.com>
2025-09-26 21:12:05 -0700

    use vmrss instead

--- platform/linux/memusage.c
+++ platform/linux/memusage.c
@@ -17,43 +17,25 @@ void memusage_init (void)
 
 int memusage (pid_t pid)
 {
-  char a[SIZE], *p, *q;
-  int data, stack;
-  int n, v, fd;
+  char path[64], line[256];
+  FILE *f;
+  int rss = -1;
 
-  p = a;
-  sprintf (p, "/proc/%d/status", pid);
-  fd = open (p, O_RDONLY);
-  if (fd < 0){
-    if (errno == ENOENT){
-      return 0;
-    } else {
-      error (NULL);
+  snprintf(path, sizeof(path), "/proc/%d/status", pid);
+  f = fopen(path, "r");
+  if (!f)
+    return -1;
+
+  while (fgets(line, sizeof(line), f))
+  {
+    if (strncmp(line, "VmRSS:", 6) == 0)
+    {
+      sscanf(line + 6, "%d", &rss);
+      break;
     }
   }
-
-  do
-    n = read (fd, p, SIZE);
-  while ((n < 0) && (errno == EINTR));
-  if (n < 0)
-    error (NULL);
-  do
-    v = close (fd);
-  while ((v < 0) && (errno == EINTR));
-  if (v < 0)
-    error (NULL);
-
-  data = stack = 0;
-  q = strstr (p, "VmData:");
-  if (q != NULL)
-    {
-      sscanf (q, "%*s %d", &data);
-      q = strstr (q, "VmStk:");
-      if (q != NULL)
-        sscanf (q, "%*s %d\n", &stack);
-    }
-
-  return (data + stack);
+  fclose(f);
+  return rss; /* in kB */
 }
 
 void memusage_close (void)
